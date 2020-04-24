5f3ca8880f83d1919adc8dd691d7aaaf0d8d67ab
Slava <slava91petrushin@yandex.ru>
2020-04-24 14:51:58 +0300

    Updates

---
+++ precache-manifest.d3b439d6922da6e39704c37c71c82591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9bdf538c8e81cc03f82e6b30abdcd85",
+    "url": "/hooks/index.html"
+  },
+  {
+    "revision": "8601ed3ff7836332c8ea",
+    "url": "/hooks/static/css/main.5babd15d.chunk.css"
+  },
+  {
+    "revision": "cb03cc414b47104c0028",
+    "url": "/hooks/static/js/2.5f426400.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hooks/static/js/2.5f426400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8601ed3ff7836332c8ea",
+    "url": "/hooks/static/js/main.9a4f2775.chunk.js"
+  },
+  {
+    "revision": "0c08b2fa7913cb3fd924",
+    "url": "/hooks/static/js/runtime-main.846bf6ec.js"
+  }
+]);
